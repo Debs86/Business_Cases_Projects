b2478291925caabacc223183e3f45a3aa377fbbe
Debs86 <debora_antunessantos@yahoo.com.br>
2021-04-19 20:39:40 +0100

    updtes

--- app.py
+++ app.py
@@ -1,28 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Business Case #3 - Market Basket Analysis
-
-# ## Authors:
-# #### Débora Santos (m20200748),Pedro Henrique Medeiros (m20200742), Rebeca Pinheiro (m20201096)
-# 
-# #### Group D - D4B Consulting
-
-# ### Installing and import packages
-# 
-# Maybe it will be necessary install some 'special' packages to this notebook works. 
-# 
-# Please follow the next cells and check if it's necessary
-
-# In[1]:
-
-
-#Install package mlxtend
-#!pip install mlxtend
-
-
-# In[2]:
-
 
 # Import packages
 import pandas as pd
@@ -41,23 +16,17 @@ import plotly.express as px
 
 # ### Collect initial data
 
-# In[3]:
-
-
 #import dataset
 path = 'https://raw.githubusercontent.com/Debs86/Business_Cases_Projects/master/BC3/'
 orders = pd.read_csv(path +'orders.csv',sep=",")
 orders_prod = pd.read_csv(path +'order_products.csv',sep=",")
 departments = pd.read_csv(path +'departments.csv',sep=",")
 products = pd.read_csv(path +'products.csv',sep=",")
-pd.set_option('display.max_rows', 500)
-pd.set_option('display.max_columns', 500)
-pd.set_option('display.width', 1000)
+
 
 
 # ### Creating the dataframes to plot the customers' behaviors
 
-# In[4]:
 
 
 #Create a function to return the number of orders by day of week or per hour of the day to be use in the app
@@ -70,14 +39,8 @@ def frequent_time(option):
     return freq
 
 
-# In[5]:
-
-
 #Create the dataframe grouped by days since prior order
 prior_orders = orders['days_since_prior_order'].value_counts()
-
-
-# In[6]:
 
 
 #Create a dataframe grouped by user id
@@ -93,9 +56,6 @@ number_orders.rename(columns={'user_id': 'number_orders'}, inplace = True)
 number_orders= number_orders.groupby("number_orders")['number_user'].agg(['count'])
 #Reset index
 number_orders = number_orders.reset_index()
-
-
-# In[7]:
 
 
 #Create a dataframe to plot the basket size in the dash
@@ -119,23 +79,13 @@ avg_basket_top25 = avg_basket[0:25]
 
 # ### Creating the dataframes to plot the Products and departments most consumed
 
-# In[8]:
-
-
 #Create a dataframe to merge the datasets by product and department
 df_merge = pd.merge(orders_prod, products, how='left', on='product_id')
 df_merge = pd.merge(df_merge, departments, how='left', on='department_id')
 
 
-# In[9]:
-
-
 #Products more frequent by itens sold
 prod_freq = df_merge['product_name'].value_counts()
-
-
-# In[10]:
-
 
 # Create dataframe to plot the number of orders by products
 
@@ -150,9 +100,6 @@ df_pivot =df_pivot.T
 df_pivot['sum'] = df_pivot[df_pivot.columns].apply(lambda x: sum(x), axis = 1)
 #Sort by number of orders. 
 df_pivot.sort_values(by='sum', ascending=False, inplace=True)
-
-
-# In[11]:
 
 
 #Departments more frequent by itens sold
@@ -172,9 +119,6 @@ grouped['department'] = grouped['department'].replace(['personal care', 'babies'
                                                       'others')
 
 
-# In[12]:
-
-
 # Create dataframe to plot the number of orders by departments
 df2 = df_merge.drop(["product_id", "department_id", 'add_to_cart_order','reordered','product_name'], axis=1)
 # Pivot the data - lines as orders and products as columns
@@ -186,10 +130,6 @@ df_pivot_dept =df_pivot_dept.T
 df_pivot_dept['sum'] = df_pivot_dept[df_pivot_dept.columns].apply(lambda x: sum(x), axis = 1)
 #Sort by number of orders. 
 df_pivot_dept.sort_values(by='sum', ascending=False, inplace=True)
-
-
-# In[13]:
-
 
 #Create a dataframe groupped by reordered or not
 prod_reord = df_merge.groupby("product_name")["reordered"].agg(['count','sum'])
@@ -206,8 +146,6 @@ prod_reord.sort_values(by='reorder_probability', ascending=False, inplace=True)
 prod_reord_top30 = prod_reord[0:30]
 
 
-# In[14]:
-
 
 #Selected products
 #The products that appears in at least 10% of orders. Represent more than 50% of total itens sold. 
@@ -218,15 +156,8 @@ for variable in list1:
     if variable in list2:
         products.append(variable)
 
-
-# In[15]:
-
-
 #Create a dataframe to have products and dow, hour_of_day in the same dataframe
 df_merge2 = pd.merge(df_merge, orders, how='left', on='order_id')
-
-
-# In[16]:
 
 
 #Create a function to return the number of orders by day of week or per hour of the day by product
@@ -242,34 +173,18 @@ def frequent_time_prod(product, option):
 # ### Heatmap for the complementary products
 
 
-
-
-# In[18]:
-
-
 # Pivot the data - lines as orders and products as columns
 pt = pd.pivot_table(df1, index='order_id', columns='product_name',
                     aggfunc=lambda x: 1 if len(x)>0 else 0).fillna(0)
 
-
-# In[19]:
-
-
 # Create new support dataframe for two products only
 #frequent_itemsets_two = apriori(pt, min_support=0.03, use_colnames=True,  max_len = 2)
-
-
-# In[20]:
-
 
 # Compute association rules for frequent_itemsets
 #rulesConfidence_two = association_rules(frequent_itemsets_two, metric="confidence", min_threshold=0.50)
 
 # High Confidence and high Lift - complementary products
 #complementary_p = rulesConfidence_two[(rulesConfidence_two['lift'] > 1.3)].sort_values('lift', ascending=False)
-
-
-# In[21]:
 
 
 # Create column with the number of the left hand side items
@@ -284,9 +199,6 @@ pt = pd.pivot_table(df1, index='order_id', columns='product_name',
 
 # Transform the DataFrame of rules into a matrix using the lift metric
 #pivot = complementary_p.pivot(index = 'consequents_', columns = 'antecedents_', values= 'lift')
-
-
-# In[22]:
 
 
 # divide the data
@@ -308,9 +220,6 @@ pt = pd.pivot_table(df1, index='order_id', columns='product_name',
 
 # ### Creating the components
 
-# In[23]:
-
-
 #Create a  Plotly Visualization to show the days since prior order - Bar plot
 #Define the dataset to be used
 df = prior_orders
@@ -327,9 +236,6 @@ layout_bar = dict(title=dict(
                   yaxis=dict(title='Number of orders'),
                   title_x=0.5)
 fig_bar = go.Figure(data=data_bar, layout=layout_bar)
-
-
-# In[24]:
 
 
 ##Create a Plotly Visualization to be included in dashboard - Bar plot
@@ -349,10 +255,6 @@ layout = dict(title=dict(text='Quantity of orders'),
 #Create the figure
 fig_users = go.Figure(data, layout)
 
-
-# In[25]:
-
-
 #Create a bar plot to show the basket size
 #define the data
 data = dict(type='bar',
@@ -369,9 +271,6 @@ layout = dict(title=dict(text='Average Basket size'),
 avgbasket_fig = go.Figure(data, layout)
 
 
-# In[26]:
-
-
 #Products plots
 #Plot number of itens sold by product - Treemap
 #Define the dataset
@@ -383,10 +282,6 @@ treemap_layout = go.Layout( {"margin": dict(t=35, b=10, l=5, r=5, pad=4)},title=
 #Create the figure
 treemap_figure = go.Figure(data=treemap_trace, layout=treemap_layout)
 
-
-# In[27]:
-
-
 #Plotly number of orders by product - Treemap
 #Define the dataset
 df = df_pivot
@@ -396,10 +291,6 @@ treemap_trace = go.Treemap(labels=df.index[0:16], parents=[""] * len(df.index), 
 treemap_layout = go.Layout({"margin": dict(t=35, b=10, l=5, r=5, pad=4)}, title=dict(text='Number of orders by product') )
 #Create the figure
 treemap_prod = go.Figure(data=treemap_trace, layout=treemap_layout)
-
-
-# In[28]:
-
 
 #Department plots
 #Plotly % of itens sold by department - Pie plot
@@ -416,9 +307,6 @@ department_layout = dict(title=dict(text='Itens sold by department - % share'))
 department_fig = go.Figure(data= department_data, layout=department_layout)
 
 
-# In[29]:
-
-
 #Filter the  deparments that appears at least in 5% of orders
 #Plotly number of orders by deparment
 #Define the dataset
@@ -429,10 +317,6 @@ treemap_trace = go.Treemap(labels=df.index[0:16], parents=[""] * len(df.index), 
 treemap_layout = go.Layout({"margin": dict(t=30, b=10, l=5, r=5, pad=4)}, title=dict(text='Number of orders by department') )
 #Create the figure
 treemap_dept = go.Figure(data=treemap_trace, layout=treemap_layout)
-
-
-# In[30]:
-
 
 #Plotly Visualization - Bar plot
 #Define the data
@@ -458,9 +342,6 @@ reordered_bar = go.Figure(data=data_bar, layout=layout_bar)
 # ### Designing the dashboard
 
 # #### Layout
-
-# In[31]:
-
 
 app = dash.Dash(__name__,external_stylesheets =[dbc.themes.BOOTSTRAP], suppress_callback_exceptions=True)
 server = app.server
@@ -564,8 +445,6 @@ app.layout = html.Div([
 
 # ### Callbacks
 
-# In[32]:
-
 
 #Plot Bar Transactions by period
 @app.callback(Output('bar_plot', 'figure'),
@@ -583,10 +462,6 @@ def update_barplot(option):
                   title_x=0.5)
     fig_bar = go.Figure(data=data_bar, layout=layout_bar)
     return fig_bar
-
-
-# In[33]:
-
 
 #Plot Bar Transactions by period by product
 @app.callback(Output('bar_plot_prod', 'figure'),
@@ -607,10 +482,6 @@ def update_barplot_prod(product, option):
                   title_x=0.5)
     fig_bar_prod = go.Figure(data=data_bar, layout=layout_bar)
     return fig_bar_prod
-
-
-# In[34]:
-
 
 if __name__ == '__main__':
     app.run_server()
